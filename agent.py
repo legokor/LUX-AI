062ac6d8971c7ad26e9939e59c5fb7fa03a0587d
Peterz0ne <honved.peti2002@gmail.com>
2024-10-14 19:21:23 +0200

    gyules 10.14

--- agent.py
+++ agent.py
@@ -1,12 +1,38 @@
 import math, sys
+import random
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 from lux.game import Game
 from lux.game_map import Cell, RESOURCE_TYPES
 from lux.constants import Constants
 from lux.game_constants import GAME_CONSTANTS
 from lux import annotate
+from simple.dqn import DQN, ReplayMemory
 
 DIRECTIONS = Constants.DIRECTIONS
 game_state = None
+
+wood_gain = 1
+coal_gain = 2
+uran_gain = 3
+city_gain = 500
+worker_gain = 7.5
+
 
 def get_input(observation, game_state, unit):
     """
@@ -16,11 +42,11 @@ def get_input(observation, game_state, unit):
     - Legközelebbi város távolsága
     - Legközelebbi város üzemanyaga
     - Legközelebbi város üzemanyag igénye
-    - Est-Nap ciklus 
+    - Est-Nap ciklus
     - Tud-e építeni a worker
     """
 
-    # Closest resource 
+    # Closest resource
     player = game_state.players[observation.player]
     width, height = game_state.map.width, game_state.map.height
 
@@ -30,7 +56,7 @@ def get_input(observation, game_state, unit):
             cell = game_state.map.get_cell(x, y)
             if cell.has_resource():
                 resource_tiles.append(cell)
-    
+
     closest_resource_dist = math.inf
     closest_resource_tile = None
 
@@ -41,7 +67,7 @@ def get_input(observation, game_state, unit):
         if dist < closest_resource_dist:
             closest_resource_dist = dist
             closest_resource_tile = resource_tile
-    
+
     # Space left
     space_left = unit.get_cargo_space_left()
 
@@ -58,14 +84,15 @@ def get_input(observation, game_state, unit):
                 closest_city_fuel = city.fuel
                 # Closest city light_upkeep
                 closest_city_light_upkeep = city.light_upkeep
-    
+
     # Day Night cycle
     cycle_position = observation["step"] % 40
 
     # Can the unit build a city
     can_build = 1 if unit.can_build(game_state.map) else 0
 
-    return [closest_resource_dist, space_left, closest_city_dist, closest_city_fuel, closest_city_light_upkeep, cycle_position, can_build]
+    return [closest_resource_dist, space_left, closest_city_dist, closest_city_fuel, closest_city_light_upkeep,
+            cycle_position, can_build]
 
 
 def agent(observation, configuration):
@@ -79,10 +106,10 @@ def agent(observation, configuration):
         game_state.id = observation.player
     else:
         game_state._update(observation["updates"])
-    
+
     actions = []
 
-    ### AI Code goes down here! ### 
+    ### AI Code goes down here! ###
     player = game_state.players[observation.player]
     opponent = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
@@ -96,9 +123,9 @@ def agent(observation, configuration):
 
     # we iterate over all our units and do something with them
     for unit in player.units:
-        
+
         # Test
-        #with open("log.txt", "a") as f:
+        # with open("log.txt", "a") as f:
         #    print(get_input(observation, game_state, unit), file=f)
 
         if unit.is_worker() and unit.can_act():
@@ -132,5 +159,77 @@ def agent(observation, configuration):
 
     # you can add debug annotations using the functions in the annotate object
     # actions.append(annotate.circle(0, 0))
-    
+
     return actions
+
+
+def get_rewards(last_game_state, last_observation, new_observation, new_game_state):
+    new_player = new_game_state.players[new_observation.player]
+    last_player = last_game_state.players[last_observation.player]
+    width, height = last_game_state.map.width, last_game_state.map.height
+
+
+def get_city_reward_per_player(new_player, last_player):
+    last_city_tiles = 0
+    for city in last_player.cities:
+        last_city_tiles += len(city.citytiles)
+    new_city_tiles = 0
+    for city in new_player.cities:
+        new_city_tiles += len(city.citytiles)
+
+    new_cities = (new_city_tiles - last_city_tiles) * city_gain
+    return new_cities
+
+
+def get_new_worker_reward_per_player(new_player, last_player):
+    last_unit_count = 0
+    for unit in last_player.units:
+        if (unit.is_worker()):
+            last_unit_count += 1
+    new_unit_count = 0
+    for unit in new_player.units:
+        if (unit.is_worker()):
+            new_unit_count += 1
+
+    new_workers = (last_unit_count - new_unit_count) * worker_gain
+    return new_workers
+
+
+def get_new_cargo_reward_per_player(new_player, last_player):
+    last_coal = 0
+    last_uran = 0
+    last_wood = 0
+    for unit in last_player.units:
+        if (unit.is_worker()):
+            last_coal += unit.cargo.coal()
+            last_uran += unit.cargo.uran()
+            last_wood += unit.cargo.wood()
+    new_coal = 0
+    new_uran = 0
+    new_wood = 0
+    for unit in new_player.units:
+        if (unit.is_worker()):
+            new_coal += unit.cargo.coal()
+            new_uran += unit.cargo.uran()
+            new_wood += unit.cargo.wood()
+
+    reward_coal = (last_coal - new_coal) * coal_gain
+    reward_uran = (last_uran - new_uran) * uran_gain
+    reward_wood = (last_wood - new_wood) * wood_gain
+    return reward_coal + reward_uran + reward_wood
+
+
+def complicated_reward_for_player(last_player):
+    last_coal = 0
+    last_uran = 0
+    last_wood = 0
+    for unit in last_player.units:
+        if (unit.is_worker()):
+            last_coal += unit.cargo.coal()
+            last_uran += unit.cargo.uran()
+            last_wood += unit.cargo.wood()
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,63 @@
 from typing import Dict
 import sys
 from agent import agent
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+from lux.game import Game
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from simple.dqn import DQN, ReplayMemory
+
 if __name__ == "__main__":
-    
+    is_ipython = 'inline' in matplotlib.get_backend()
+    if is_ipython:
+        from IPython import display
+
+    plt.ion()
+    # env = gym.make("CartPole-v1")
+    # if GPU is to be used
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else
+        "mps" if torch.backends.mps.is_available() else
+        "cpu"
+    )
+    Transition = namedtuple('Transition',
+                            ('state', 'action', 'next_state', 'reward'))
+
+    n_actions = 8
+    # n_actions = env.action_space.n
+    n_observations = 8
+    # state, info = env.reset()
+    # n_observations = len(state)
+    BATCH_SIZE = 128
+    GAMMA = 0.99
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 1000
+    TAU = 0.005
+    LR = 1e-4
+    policy_net = DQN(n_observations, n_actions).to(device)
+    target_net = DQN(n_observations, n_actions).to(device)
+    target_net.load_state_dict(policy_net.state_dict())
+
+    optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+    memory = ReplayMemory(10000)
+    state = random.sample(range(1, 50), 8)
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+
+
     def read_input():
         """
         Reads input from stdin
@@ -11,24 +66,44 @@ if __name__ == "__main__":
             return input()
         except EOFError as eof:
             raise SystemExit(eof)
+
+
     step = 0
+
+
     class Observation(Dict[str, any]):
         def __init__(self, player=0) -> None:
             self.player = player
             # self.updates = []
             # self.step = 0
+
+
     observation = Observation()
     observation["updates"] = []
     observation["step"] = 0
     player_id = 0
+    last_map_state = None
     while True:
         inputs = read_input()
         observation["updates"].append(inputs)
-        
+
         if step == 0:
             player_id = int(observation["updates"][0])
             observation.player = player_id
         if inputs == "D_DONE":
+            global game_state
+
+            ### Do not edit ###
+            if observation["step"] == 0:
+                game_state = Game()
+                game_state._initialize(observation["updates"])
+                game_state._update(observation["updates"][2:])
+                game_state.id = observation.player
+            else:
+                game_state._update(observation["updates"])
+
+            possible_actions = []
+
             actions = agent(observation, None)
             observation["updates"] = []
             step += 1
